f8a62772106a7be64e0d2767dd64e82267d1eb32
shk001 <296155457@qq.com>
2024-06-24 13:28:25 +0800

    123

---
+++ src/main/java/neo4j/constants/NsConstant.java
@@ -0,0 +1,36 @@
+package neo4j.constants;
+
+public class NsConstant {
+
+    public final static String oslc_ex_prefix = "oslc_ex";
+    public final static String oslc_ex_namespace = "localhost:8082/BLOCK-vocab#";
+    public final static String oslc_exs_prefix = "oslc_exs";
+    public final static String oslc_exs_namespace = "localhost:8082/BLOCK-shape#";
+    public final static String OSLC_BLOCK_SHAPE= "http://localhost:8080/BLOCK/ResourceShape";
+    public final static String LDP_PREFIX="ldp";
+    public final static String LDP_NAMESPACE="http://www.w3.org/ns/ldp#";
+
+
+
+
+    //------------------------------CLASS------------------------------
+    public final static String BLOCK_NAMESPACE = "localhost:8082/BLOCK-vocab#BLOCK";
+
+
+
+    //----------------------------Property------------------------------
+    public final static String BLOCK_ID = oslc_ex_namespace + "id";
+    public final static String BLOCK_NAME = oslc_ex_namespace + "name";
+    public final static String BLOCK_XMI_TYPE = oslc_ex_namespace + "XmiType";
+    public final static String BLOCK_XMI_ID = oslc_ex_namespace + "XmiId";
+    public final static String BLOCK_VISIBILITY = oslc_ex_namespace + "visibility";
+    public final static String oslcID = oslc_ex_prefix + "id";
+    public final static String oslc_NAME = oslc_ex_prefix + "name";
+    public final static String oslc_XMI_TYPE = oslc_ex_prefix + "XmiType";
+    public final static String oslc_XMI_ID = oslc_ex_prefix + "XmiId";
+    public final static String oslc_VISIBILITY = oslc_ex_prefix + "visibility";
+
+
+
+
+}
